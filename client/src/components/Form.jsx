7d24894079abf8395ec96d9e3e0b0911ff61a6bb
siddhardha varma <sid@pop-os.localdomain>
2022-12-09 11:10:23 +0530

    done

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import axios from 'axios'
-import { Input,Container,Button,Select } from '@chakra-ui/react'
-import { useState } from 'react'
-import Display from './Display';
+import { Input,Container,Button,Select,Spinner } from '@chakra-ui/react'
+import { useState} from 'react'
 const Form = () => {
-   
+  
    const [prompt,setPrompt] = useState('')
+   const [count,setCount] = useState()
    const [size,setSize] = useState('')
-   const [loading,setLoading] = useState()
-   const [postdata,setPostdata] = useState({})
+   const [loading,setLoading] = useState(false)
+   const [postdata,setPostdata] = useState()
    const handleSubmit = (e)=>{
         e.preventDefault()
-        setLoading(true)
-        axios.post('http://localhost:3001/openai/generateimage',{
-            prompt,
-            size,
-            "count" : 5,
-        }).then((res) => {
-            setPostdata(res.data)
-            console.log(res.data)
-            setLoading(false)
-         })
-        .catch(error => console.log(error))  
+        setLoading(true) 
+        axios.post('http://localhost:3001/openai/generateimage',{prompt,size,count}).then((res) => {
+         setPostdata(res.data)
+         // console.log(res.data)
+         setLoading(false)
+      })
+     .catch(error => console.log(error)) 
    }
+   if(postdata){
+      console.log(postdata)
+   }
+  
   return (
     
       <div className='max-w-full p-10 justify-center place-content-center   m-auto'> 
@@ -32,7 +32,7 @@ const Form = () => {
          <Input placeholder="prompt" value={prompt} onChange={(e)=>{
                setPrompt(e.target.value)
          }} />
-         <div className='py-5'>
+         <div className='py-5 flex'>
          <Select placeholder='size'value={size} onChange={(e)=>{
                setSize(e.target.value)
          }}  >
@@ -40,19 +40,21 @@ const Form = () => {
             <option value='medium'>medium</option>
             <option value='large'>large</option>
          </Select>
+         <Input placeholder="count" value={count} type="number" onChange={(e)=>{
+               setCount(Number(e.target.value))
+         }} />
          </div>
          <div className='py-5'>
             <Button type="submit"  >generate</Button>
          </div>       
-          <p>{"iosrghi" + loading}</p>
          </Container >
          </form>
-         <div>
-            {postdata && postdata.data.map((gg)=>{
-                <>
-                   <img src={gg.url} alt="" />
-                </>
-            })}
+         <div className='flex flex-wrap gap-10 items-center justify-center'>
+            {loading == false ? postdata && postdata.data.map((item)=>(
+               <div className='p-5'> 
+                 <img src={item.url} alt="image" />
+               </div>
+            )) : <Spinner />}
          </div>
         
       </div>
